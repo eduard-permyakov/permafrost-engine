b2448e53d243a84ae1825a30e2afc00a517a766c
Eduard Permyakov <edward.permyakov@gmail.com>
2018-01-07 19:45:20 -0500

    Add license headers to new files

--- src/map/public/tile.h
+++ src/map/public/tile.h
@@ -1,3 +1,22 @@
+/*
+ *  This file is part of Permafrost Engine. 
+ *  Copyright (C) 2018 Eduard Permyakov 
+ *
+ *  Permafrost Engine is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Permafrost Engine is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
 #ifndef TILE_H
 #define TILE_H
 
@@ -21,6 +40,7 @@ enum tiletype{
     TILETYPE_RAMP_NS           = 2,
     TILETYPE_RAMP_EW           = 3,
     TILETYPE_RAMP_WE           = 4,
+    /* For corners, the direction in the name is that of the central lowered corner */
     TILETYPE_CORNER_CONCAVE_SW = 5,
     TILETYPE_CORNER_CONVEX_SW  = 6,
     TILETYPE_CORNER_CONCAVE_SE = 7,
