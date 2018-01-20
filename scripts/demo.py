e29425d7b0cd2def9b13ed473b56333f868760ab
Eduard Permyakov <edward.permyakov@gmail.com>
2018-01-20 14:31:22 -0500

    Add oak tree asset to repository and to demo

--- scripts/demo.py
+++ scripts/demo.py
@@ -4,13 +4,20 @@ pf.set_ambient_light_color([1.0, 1.0, 1.0])
 pf.set_emit_light_color([1.0, 1.0, 1.0])
 pf.set_emit_light_pos([0.0, 300.0, 0.0])
 
-pf.new_game("./assets/maps/grass-cliffs-1", "grass-cliffs.pfmap", "grass-cliffs.pfmat")
+pf.new_game("assets/maps/grass-cliffs-1", "grass-cliffs.pfmap", "grass-cliffs.pfmat")
 
-sinbad = pf.AnimEntity("assets/models/sinbad", "Sinbad.pfobj", "Sinbad", "IdleBase")
+sinbad = pf.AnimEntity("assets/models/sinbad", "Sinbad.pfobj", "Sinbad", "Dance")
+sinbad.pos = [0.0, 8.0, -50.0]
+sinbad.scale = [1.0, 1.0, 1.0]
 sinbad.activate()
 
-sinbad.pos = [0.0, 5.0, -50.0]
-sinbad.scale = [1.0, 1.0, 1.0]
+oak_tree = pf.Entity("assets/models/oak_tree", "oak_tree.pfobj", "OakTree")
+oak_tree.pos = [30.0, 8.0, -50.0]
+oak_tree.scale = [2.0, 2.0, 2.0]
+oak_tree.activate()
 
-sinbad.play_anim("Dance")
+oak_leafless = pf.Entity("assets/models/oak_tree", "oak_leafless.pfobj", "OakLeafless")
+oak_leafless.pos = [0.0, 8.0, -10.0]
+oak_leafless.scale = [1.5, 1.5, 1.5]
+oak_leafless.activate()
 
