35346a36458868e3515b456f7c0282a1b39078f9
Eduard Permyakov <edward.permyakov@gmail.com>
2018-06-13 00:29:00 +0300

    Bump to version 0.17; update docs

--- src/main.c
+++ src/main.c
@@ -42,7 +42,7 @@
 
 
 #define PF_VER_MAJOR 0
-#define PF_VER_MINOR 16
+#define PF_VER_MINOR 17
 #define PF_VER_PATCH 0
 
 /*****************************************************************************/
