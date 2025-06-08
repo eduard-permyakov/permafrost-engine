5eef6a09a3883839d7b5a18815ab4b75d6b4e04f
Eduard Permyakov <edward.permyakov@gmail.com>
2025-06-08 18:43:39 +0300

    timer_events: Change period event order

--- src/game/timer_events.c
+++ src/game/timer_events.c
@@ -88,23 +88,23 @@ static void timer_60hz_handler(void *unused1, void *unused2)
 {
     s_num_60hz_ticks++;
 
-    if(s_num_60hz_ticks % 2 == 0)
-        E_Global_Notify(EVENT_30HZ_TICK, NULL, ES_ENGINE);
-
-    if(s_num_60hz_ticks % 3 == 0)
-        E_Global_Notify(EVENT_20HZ_TICK, NULL, ES_ENGINE);
-
-    if(s_num_60hz_ticks % 4 == 0)
-        E_Global_Notify(EVENT_15HZ_TICK, NULL, ES_ENGINE);
-
-    if(s_num_60hz_ticks % 6 == 0)
-        E_Global_Notify(EVENT_10HZ_TICK, NULL, ES_ENGINE);
+    if(s_num_60hz_ticks % 60 == 0)
+        E_Global_Notify(EVENT_1HZ_TICK, NULL, ES_ENGINE);
 
     if(s_num_60hz_ticks % 12 == 0)
         E_Global_Notify(EVENT_5HZ_TICK, NULL, ES_ENGINE);
 
-    if(s_num_60hz_ticks % 60 == 0)
-        E_Global_Notify(EVENT_1HZ_TICK, NULL, ES_ENGINE);
+    if(s_num_60hz_ticks % 6 == 0)
+        E_Global_Notify(EVENT_10HZ_TICK, NULL, ES_ENGINE);
+
+    if(s_num_60hz_ticks % 4 == 0)
+        E_Global_Notify(EVENT_15HZ_TICK, NULL, ES_ENGINE);
+
+    if(s_num_60hz_ticks % 3 == 0)
+        E_Global_Notify(EVENT_20HZ_TICK, NULL, ES_ENGINE);
+
+    if(s_num_60hz_ticks % 2 == 0)
+        E_Global_Notify(EVENT_30HZ_TICK, NULL, ES_ENGINE);
 }
 
 /*****************************************************************************/
